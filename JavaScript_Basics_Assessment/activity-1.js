c6831450fbb66c39d536a9fca73cbb83d7cb4209
Rubai Danish <Rubydubydoo@Rubais-MBP.attlocal.net>
2021-02-18 00:17:55 -0600

    first commit

---
+++ JavaScript_Basics_Assessment/activity-1.js
@@ -0,0 +1,31 @@
+//1. ANSWER 81
+
+var num1 = 9;
+var num2 = 9;
+console.log(num1 * num2);
+
+
+//2. ANSWER 4.666666666666667
+
+var num3 = 56;
+var num4 = 12;
+console.log(num3 / num4);
+
+//3. ANSWER 2
+var num5 = 281;
+var num6 = 9;
+console.log(num5 % num6);
+
+//4. ANSWER Infinity
+
+var num7 = 18;
+var num8 = 0;
+console.log(num7 / num8);
+
+//5. ANSWER 148
+
+'56' + '92';
+
+//6. ANSWER false
+
+42 === 52;
